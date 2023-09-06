db2cf5effc530332d58510e1f0ee43043a95deed
mostafa-cse <100847323+mostafa-cse@users.noreply.github.com>
2023-09-06 11:16:18 +0600

    A+B

---
+++ 1000.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define int long long
+using namespace std;
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<nl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    int testcase = 1;
+    //cin >> testcase;
+
+    for (int tcase = 1; tcase <= testcase; ++tcase){
+        //cout << "Case #" << tcase << ": ";
+        solve();
+    }
+    return 0;
+}
