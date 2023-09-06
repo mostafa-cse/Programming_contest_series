754c5bf248be0b33b1c344a3533093eab405e779
mostafa-cse <100847323+mostafa-cse@users.noreply.github.com>
2023-09-06 11:19:07 +0600

    1264

---
+++ 1264_Workdays.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define int long long
+using namespace std;
+void solve(){
+    int n,m;
+    cin>>n>>m;
+
+    cout<<(n*(m+1))<<nl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    int testcase = 1;
+    //cin >> testcase;
+
+    for (int tcase = 1; tcase <= testcase; ++tcase){
+        //cout << "Case #" << tcase << ": ";
+        solve();
+    }
+    return 0;
+}
